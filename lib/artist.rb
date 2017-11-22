a7f2b53e18a3c5ac31a58e36f5257d70cee28c1f
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-22 02:42:32 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,7 +9,7 @@ class Artist
   def add_song(song)
     @songs << song
   end
-  
+
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
--- lib/author.rb
+++ lib/author.rb
@@ -6,8 +6,6 @@ class Author
   end
 
   def add_post(post)
-    post = Post.new
-    post.author = self
     @posts << post
   end
 
