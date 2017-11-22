cb40f3ce31a7070dcaf321c1f285b1d32f239616
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-22 03:09:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,15 @@
 class Song
-  attr_accessor :artist, :name
+  attr_accessor :artist, :name, :artist_name
 
   def initialize(name)
     @name = name
   end
 
   def artist_name
-    self.artist.name if @artist == Artist
+    if defined? @artist
+      self.artist.name
+    else
+      nil
+    end
   end
 end
