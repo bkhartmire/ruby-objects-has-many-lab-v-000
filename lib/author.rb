4ca4c1a9c6e15cdf192f1b54dd496e9e4599fae9
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-22 02:38:18 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,4 +1,27 @@
 class Author
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    post = Post.new
+    post.author = self
+    @posts << post
+  end
+      
   def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+    @posts << post
+  end
+
+  def posts
+    @posts
+  end
+
+  def post_count
+    @posts.uniq.length
   end
 end
