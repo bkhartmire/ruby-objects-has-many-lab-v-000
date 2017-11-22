37952315bf9db6a470a4feface3214b504616aaa
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-22 03:02:08 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,13 @@
 class Post
-  attr_accessor :author, :title
+  attr_accessor :author, :title, :author_name
 
   def initialize(title)
     @title = title
   end
 
   def author_name
-    @author
+    @author = author
+    @author_name = author.name
+    @author_name
   end
 end
